d1c846e7591f1b004b9da790706f6dba204d8111
Eric Oriez <ericoriez@live.fr>
2024-09-04 10:21:15 +0200

    Gestion des dates avec timeago.js

--- src/views/CitiesList.vue
+++ src/views/CitiesList.vue
@@ -1,49 +1,60 @@
 <template>
-    <div>
-      <h1>Liste des Villes</h1>
-      <div v-for="city in cities" :key="city.id" class="city-container">
-        <city
-          :name="city.name"
-          :weather="city.weather"
-          :temperature="city.temperature"
-          :updated-at="city.updatedAt"
-        ></city>
-      </div>
+  <div>
+    <h1>Liste des Villes</h1>
+    <div v-for="city in cities" :key="city.id" class="city-container">
+      <city
+        :name="city.name"
+        :weather="city.weather"
+        :temperature="city.temperature"
+        :updated-at="formatDate(city.updatedAt)"
+      ></city>
     </div>
-  </template>
-  
-  <script>
-  import City from '../components/City.vue';
-  
-  export default {
-    components: {
-      City
-    },
-    data() {
-      return {
-        cities: [
-          {
-            id: 1,
-            name: 'Ville 1',
-            weather: 'Ensoleillé',
-            temperature: 22.0,
-            updatedAt: new Date()
-          },
-          {
-            id: 2,
-            name: 'Ville 2',
-            weather: 'Peu nuageux',
-            temperature: 19.5,
-            updatedAt: new Date()
-          }
-        ]
-      };
+  </div>
+</template>
+
+<script>
+import City from '../components/City.vue';
+import { format, register } from 'timeago.js';
+import fr from 'timeago.js/lib/lang/fr';
+
+export default {
+  components: {
+    City
+  },
+  data() {
+    return {
+      cities: [
+        {
+          id: 1,
+          name: 'Ville 1',
+          weather: 'Ensoleillé',
+          temperature: 22.0,
+          updatedAt: new Date('2023-06-12')
+        },
+        {
+          id: 2,
+          name: 'Ville 2',
+          weather: 'Peu nuageux',
+          temperature: 19.5,
+          updatedAt: new Date()
+        }
+      ]
+    };
+  },
+  methods: {
+    formatDate(time) {
+      return format(time, 'fr_FR');
     }
+  },
+  mounted() {
+    register('fr_FR', fr); 
   }
-  </script>
-  
-  <style scoped>
-  .city-container {
-    margin-bottom: 20px;
-  }
-  </style>
+
+}
+</script>
+
+<style scoped>
+.city-container {
+  margin-bottom: 20px;
+}
+</style>
